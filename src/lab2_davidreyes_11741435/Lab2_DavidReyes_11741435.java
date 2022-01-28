bcc4e8019661edc5f90de8c7ffd624f9588805fb
davidreyesd13 <davidreyesd13@gmail.com>
2022-01-28 14:44:46 -0600

    animales defecto

--- src/lab2_davidreyes_11741435/Lab2_DavidReyes_11741435.java
+++ src/lab2_davidreyes_11741435/Lab2_DavidReyes_11741435.java
@@ -1,9 +1,20 @@
 
 package lab2_davidreyes_11741435;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab2_DavidReyes_11741435 {
     public static void main(String[] args) {
-        
+        Animal cl=new Animal();
+        Scanner sc = new Scanner(System.in);
+        int opcion; 
+        ArrayList lista=new ArrayList();
+        lista.add(new Animal("Pantera", "Pantera", "Selva", "Serpiente", "Peluda,pelo negro,ojos amarillos", "Amazonas", 100));
+        lista.add(new Animal("Cocodrilo", "Cocodrilo", "selva", "Pantera", "Tiene cara blanca", "Amazonas", 250));
+        lista.add(new Animal("Serpiente", "serpiente de rio","Selva", "Mono", "Se arrastran", "Amazonas", 75));
+        System.out.println("1.Crear animal\n2.Listar animales\n3.Eliminar animales\n4.Modificar animales\n5.Alimentar animales");
+        opcion = sc.nextInt();
     }
     
 }
